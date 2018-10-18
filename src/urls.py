4c804f6d0e7052e662d9212dc6094bab7ee30809
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-10-17 23:30:09 -0300

    Tentando de novo

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from src.classroom.views import classroom, students, teachers
 
 urlpatterns = [
@@ -8,4 +11,4 @@ urlpatterns = [
     path('accounts/signup/', classroom.SignUpView.as_view(), name='signup'),
     path('accounts/signup/student/', students.StudentSignUpView.as_view(), name='student_signup'),
     path('accounts/signup/teacher/', teachers.TeacherSignUpView.as_view(), name='teacher_signup'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
