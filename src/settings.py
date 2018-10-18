04dde3dcac18ee394de3dbb2d67ed717dd0337f9
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-10-17 23:28:45 -0300

    Trying

--- src/settings.py
+++ src/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'src/static'),
 ]
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
